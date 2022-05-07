e5979bd85a43a97ca4950fa711670c642e65653e
Richard Clarke <freshnewrich@gmail.com>
2022-05-07 15:58:22 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
+import {useState, useEffect} from "react"
+
+
 
 function App() {
+  const API_KEY="46f634da"
+  const [movie, setMovie] = useState(null)
+  const getMovie = async (searchTerm) => {
+    //here we make the api call
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${API_KEY}&t=${searchTerm}`
+    )
+    //convert response to json
+    //response has all kinds of extra stuff added so we use this to just get waht we need
+    const data = await response.json()
+    //updating the state
+    // console.log(data)
+    setMovie(data)
+      // console.log("movie",movie)
+  }
+
+  console.log(movie)
+
+  // useEffect takes a function and an array 
+  //the function will run once when the component loads ONLY
+  //will only repeat if any value in the array changes
+
+  const movieList = ["Snatch", "71", "Fight Club", "Pulp Fiction"]
+  const randomNum = Math.floor(Math.random()*movieList.length)
+  useEffect(() => {getMovie(movieList[randomNum])}, [])
+
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* //pass the form the getMovie function as a prop */}
+    <Form getMovie={getMovie}/>
+    <MovieDisplay  movie={movie}/>
     </div>
   );
+
 }
 
 export default App;
+
+// App()
